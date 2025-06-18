747dce232975dab87917e1fc670ed8627e67c0a1
Ana Beatriz Stahl <ana.beatriz.stahl@gmail.com>
2025-06-18 03:24:08 -0300

    feat: add db connection with users

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -8,16 +8,16 @@ import {
 } from '../schemas/user.schema'
 import { randomUUID, randomBytes } from 'crypto'
 import { genSalt, hash } from 'bcrypt'
+import { knex } from '../database'
+import { K } from 'vitest/dist/chunks/reporters.d.BFLkQcL6.js'
 
 export async function createUser(
   data: CreateUserInput,
 ): Promise<UserWithoutSensitiveInfoSchema> {
   const { name, email, password } = data
 
-  const userExists = usersTableSim.find((user) => user.email === email)
-  if (userExists) {
-    throw new Error('User already exists')
-  }
+  const userExists = await knex('users').where({ email }).select('*').first()
+  if (userExists) throw new Error('User already exists')
 
   const salt = await genSalt()
   const hasedPassword = await hash(password, salt)
@@ -34,64 +34,114 @@ export async function createUser(
     created_at: dateNow,
   }
 
-  usersTableSim.push(newUser)
+  await knex('users').insert(newUser)
 
   const { password_hash, password_salt, ...newUserWithoutPassword } = newUser
   return newUserWithoutPassword
 }
 
-export async function listUsers(): Promise<UserWithoutSensitiveInfoSchema[]> {
-  return usersTableSim
-    .filter((u) => !u.is_deleted)
-    .map(({ password_hash, password_salt, ...rest }) => rest)
+export async function listUsers() {
+  const users = await knex('users')
+    .where('is_deleted', false)
+    .select(
+      'user_id',
+      'name',
+      'email',
+      'is_deleted',
+      'updated_at',
+      'created_at',
+    )
+  const transformedUsers = users.map((row) => ({
+    ...row,
+    is_deleted: Boolean(row.is_deleted),
+    updated_at: new Date(row.updated_at),
+    created_at: new Date(row.created_at),
+  }))
+  return transformedUsers
 }
 
 export async function getUserById(
   user_id: string,
 ): Promise<UserWithoutSensitiveInfoSchema> {
-  const user = findUserById(user_id)
+  const user = await findUserById(user_id)
 
   if (!user) throw new Error('User not found')
 
-  const { password_hash, password_salt, ...userWithoutPassowrd } = user
-  return userWithoutPassowrd
+  const transformedUser = {
+    ...user,
+    is_deleted: Boolean(user.is_deleted),
+    updated_at: new Date(user.updated_at),
+    created_at: new Date(user.created_at),
+  }
+  const { password_hash, password_salt, ...userWithoutPassword } =
+    transformedUser
+  return userWithoutPassword
 }
 
 export async function updateUser(
   user_id: string,
   data: Partial<UpdateUserInput>,
 ): Promise<UserWithoutSensitiveInfoSchema> {
-  const user = findUserById(user_id)
+  const user = await findUserById(user_id)
 
   if (!user) throw new Error('User not found')
 
-  if (data.name) user.name = data.name
-  if (data.email) user.email = data.email
+  if (data.name)
+    await knex('users').where('user_id', user_id).update('name', data.name)
+  if (data.email)
+    await knex('users').where('user_id', user_id).update('email', data.email)
   if (data.password) {
     const salt = await genSalt()
-    user.password_hash = await hash(data.password, salt)
-    user.password_salt = salt
+    const hasedPassword = await hash(data.password, salt)
+    await knex('users')
+      .where('user_id', user_id)
+      .update('password_hash', hasedPassword)
+      .update('password_salt', salt)
   }
+  await knex('users').where('user_id', user_id).update('updated_at', new Date())
+  const updatedUser = await knex('users')
+    .where('user_id', user_id)
+    .select('*')
+    .first()
 
-  const { password_hash, password_salt, ...userWithoutPassword } = user
+  const transformedUser = {
+    ...updatedUser,
+    is_deleted: Boolean(updatedUser.is_deleted),
+    updated_at: new Date(updatedUser.updated_at),
+    created_at: new Date(updatedUser.created_at),
+  }
+  const { password_hash, password_salt, ...userWithoutPassword } =
+    transformedUser
   return userWithoutPassword
 }
 
 export async function deleteUser(
   user_id: string,
 ): Promise<UserWithoutSensitiveInfoSchema> {
-  const user = findUserById(user_id)
+  const user = await findUserById(user_id)
 
   if (!user) throw new Error('User not found')
 
-  user.is_deleted = true
+  await knex('users').where('user_id', user_id).update('is_deleted', true)
+  await knex('users').where('user_id', user_id).update('updated_at', new Date())
+  const deleted = await knex('users')
+    .where('user_id', user_id)
+    .select('*')
+    .first()
 
-  const { password_hash, password_salt, ...userWithoutPassword } = user
+  const transformedUser = {
+    ...deleted,
+    is_deleted: Boolean(deleted.is_deleted),
+    updated_at: new Date(deleted.updated_at),
+    created_at: new Date(deleted.created_at),
+  }
+  const { password_hash, password_salt, ...userWithoutPassword } =
+    transformedUser
   return userWithoutPassword
 }
 
-function findUserById(user_id: string) {
-  return usersTableSim.find(
-    (user) => user.user_id === user_id && !user.is_deleted,
-  )
+async function findUserById(user_id: string) {
+  return await (
+    await knex('users')
+  ).find((u) => u.user_id === user_id && !u.is_deleted)
 }
